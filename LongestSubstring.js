1460947a0197b8232f8876d8506d156b46b8bb45
MadMax0312 <thahirmuhammedap@gmail.com>
2024-02-28 21:19:22 +0530

    longest same substring

--- LongestSubstring.js
+++ LongestSubstring.js
@@ -5,7 +5,7 @@ let maxChar = str[0];
 let maxCount = 1;
 let result = '';
 
-for (let i = 1; i < str.length; i++) {
+for (let i = 1; i <= str.length; i++) {
   if (str[i] === currentChar) {
     currentCount++;
   } else {
@@ -18,13 +18,37 @@ for (let i = 1; i < str.length; i++) {
   }
 }
 
-if (currentCount > maxCount) {
-  maxCount = currentCount;
-  maxChar = currentChar;
-}
 
 for (let i = 0; i < maxCount; i++) {
   result += maxChar;
 }
 
 console.log(result);
+
+
+
+// const str = 'aaaabbbbbbbbbccccccccccccdddddddddddddddddd';
+
+// let s = str[0];
+// let max = 0;
+// let curr = 0;
+// let char = '';
+// let maxChar = '';
+
+// for (let i = 0; i <= str.length; i++) {
+//   if (str[i] === s) {
+//     curr++;
+//     char += s;
+//   } else {
+//     if (curr > max) {
+//       max = curr;
+//       maxChar = char;
+//     }
+//     s = str[i];
+//     char = s;
+//     curr = 1;
+//   }
+// }
+
+// console.log(maxChar);
+
